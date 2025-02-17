46f159004c8fdea5105c4bb1a6972e116ca47f51
ramesh-dharmalingam <59630992+ramesh-dharmalingam@users.noreply.github.com>
2025-02-16 19:13:58 -0500

    Update eventcatalog.config.js

--- eventcatalog.config.js
+++ eventcatalog.config.js
@@ -9,7 +9,7 @@ export default {
   trailingSlash: false,
   // Change to make the base url of the site different, by default https://{website}.com/docs,
   // changing to /company would be https://{website}.com/company/docs,
-  base: '/eventcatalog',
+  base: '/ramesh-dharmalingam.github.io/eventcatalog',
   // Customize the logo, add your logo to public/ folder
   logo: {
     alt: 'EventCatalog Logo',
